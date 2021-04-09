14922fe1677f39dc83e1b35044ce86e906ee3cd4
rulomo <rulomo@yahoo.es>
2021-04-09 23:40:32 +0200

    Update GpsMainActivity.java
    prueba

--- gpslogger/src/main/java/com/mendhak/gpslogger/GpsMainActivity.java
+++ gpslogger/src/main/java/com/mendhak/gpslogger/GpsMainActivity.java
@@ -15,6 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with GPSLogger for Android.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 package com.mendhak.gpslogger;
